78c76ceadb1f00208ef863862f04ad98e50e567f
Marco Damaceno <maadamaceno@gmail.com>
2023-12-19 00:29:25 -0300

    initiate database

--- config/sport_score/db.go
+++ config/sport_score/db.go
@@ -3,18 +3,21 @@ package config
 import (
 	"log"
 
+	"github.com/mdmaceno/sport_score/app/sport_score/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func Db() *gorm.DB {
-	url := "host=localhost user=postgres password=postgres dbname=sport_score port=5432 sslmode=disable TimeZone=America/Sao_Paulo"
+func InitDB() *gorm.DB {
+	url := "host=localhost user=postgres password=postgres dbname=sport_score_development port=5432 sslmode=disable TimeZone=America/Sao_Paulo"
 
 	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	db.AutoMigrate(&models.Country{})
 
 	return db
 }

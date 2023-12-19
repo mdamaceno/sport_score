b13004e83198e183bb7639ce8b3f5ff92a47d451
Marco Damaceno <maadamaceno@gmail.com>
2023-12-19 13:29:04 -0300

    chore: created_at and updated_at not null

--- app/models/country.go
+++ app/models/country.go
@@ -9,6 +9,6 @@ import (
 type Country struct {
 	Id        uuid.UUID `gorm:"type:uuid;primary_key;" json:"id"`
 	Name      string    `gorm:"type:varchar(255);not null;" json:"name"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	CreatedAt time.Time `gorm:"not null;" json:"created_at"`
+	UpdatedAt time.Time `gorm:"not null;" json:"updated_at"`
 }

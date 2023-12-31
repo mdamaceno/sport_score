f36de55ecd012dcbd7ebd625ca7331df6429f9ac
Marco Damaceno <maadamaceno@gmail.com>
2023-12-31 14:12:39 -0300

    refactor: move routes to app package

--- main.go
+++ main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
-	config "github.com/mdmaceno/sport_score/config"
+	"github.com/mdmaceno/sport_score/app"
+	"github.com/mdmaceno/sport_score/config"
 )
 
 func main() {
 	e := echo.New()
 	env := config.Envs()
 	DB := config.InitDB(env)
-	routes := config.InitRoutes(e, DB)
+	routes := app.InitRoutes(e, DB)
 
 	e.Logger.Fatal(routes.Start(":1323"))
 }
